24f88a23f7cb791df9076024cd79b8609b562cd1
Sxmmy123 <smichmev@gmail.com>
2025-09-19 16:26:45 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,29 +3,39 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 ];
 
-// Instalar y guardar en caché
-self.addEventListener("install", (event) => {
+// Instalación
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-// Activar y limpiar caché vieja
-self.addEventListener("activate", (event) => {
+// Activación
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
     )
   );
+  self.clients.claim();
 });
 
-// Interceptar fetch
-self.addEventListener("fetch", (event) => {
+// Fetch (offline)
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
   );
 });
+
